cb3ba8d157b27e633d6f479a58b30690aa850c43
blue103 <blue103@foxmail.com>
2023-02-08 16:48:13 +0800

    项目初始化完成

--- src/main.js
+++ src/main.js
@@ -6,6 +6,17 @@ import store from './store'
 // 加载全局样式
 import './styles/index.less'
 
+// 加载vant组件库
+import Vant from 'vant'
+
+// 加载vant全局样式
+import 'vant/lib/index.css'
+
+import 'amfe-flexible'
+
+// 注册组件
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
